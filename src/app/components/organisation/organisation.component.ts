6b86bd30804253e214ec6056c300d7922e3c4d3a
Azeem Khan <azeem@defindia.net>
2024-11-04 09:59:44 +0530

    Login UI Issue fixed.

--- src/app/components/organisation/organisation.component.ts
+++ src/app/components/organisation/organisation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faL } from '@fortawesome/free-solid-svg-icons';
 import { ChartConfiguration, ChartType } from 'chart.js';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-organisation',
   templateUrl: './organisation.component.html',
