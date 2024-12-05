dc01717083081a4838dd1b7d8e4c58956d654967
Azeem Khan <azeem@defindia.net>
2024-12-05 15:03:57 +0530

    added files in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: true,
   baseAuthApiUrl: "https://motherappauthservice.azurewebsites.net/authenticate",
   baseServiceUrl: "https://motherappserviceapi.azurewebsites.net/api/Service/Get",
-  baseSPUrl:"https://motherappserviceapi.azurewebsites.net/api/ServicePillar/Get"
+  baseSPUrl:"https://motherappserviceapi.azurewebsites.net/api/ServicePillar/Get",
+  //baseSPUrl: "https://motherappserviceapi.azurewebsites.net/api/ServicePillar/Get",
+  baseRoleMasterApiUrl: "https://motherappserviceapi.azurewebsites.net/api/RoleMaster/Get"
   // baseServicePillarUrl: "https://motherappserviceapi.azurewebsites.net/api/ServicePillar/Get"
 };
