090cc6335e270b5c607e4ce8a6f8466452d05f85
Azeem Khan <azeem@defindia.net>
2024-12-09 18:04:12 +0530

    project page issue fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DragDropModule } from '@angular/cdk/drag-drop'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -53,7 +53,10 @@ import { AllprojectsComponent } from './components/allprojects/allprojects.compo
     ReactiveFormsModule,
     HttpClientModule,
     NgChartsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,    
+    DragDropModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
