438e902e6d2d750e26b873a3c3f51da79f193998
Pallavi Bose <pallu.bose@gmail.com>
2025-03-16 18:35:33 +0530

    correct urls

--- src/app/services/rolemaster.service.ts
+++ src/app/services/rolemaster.service.ts
@@ -46,8 +46,8 @@ export class RolemasterService {
 
   getCustomRoleDefinitionForCompany(companyRoleId: number): Observable<any> {
     if (this.customRoleSettings?.companyRoleId !== companyRoleId) {
-     return this.http.get<any>("https://localhost:7047/api/CompanyUserRoleMaster/CompanyRoleSettings/"+companyRoleId).pipe(
-    //  return this.http.get<any>(environment.companyUserRoleMasterBaseUrl + `/CompanyRoleSettings/${companyRoleId}`).pipe(
+    // return this.http.get<any>("https://localhost:7047/api/CompanyUserRoleMaster/CompanyRoleSettings/"+companyRoleId).pipe(
+      return this.http.get<any>(environment.companyUserRoleMasterBaseUrl + `/CompanyRoleSettings/${companyRoleId}`).pipe(
         catchError((error) => {
           console.error('API call failed:', error);
           // Return hardcoded fallback data
@@ -61,8 +61,8 @@ export class RolemasterService {
 
   getCustomRoleForCompany(): Observable<any[]> {
     console.log(this.userRoleSettings.companyRoleId);
-   // return this.http.get<any>(environment.companyUserRoleMasterBaseUrl + "/CompanyRoles/"+this.userRoleSettings.companyId).pipe(
-    return this.http.get<any>("https://localhost:7047/api/CompanyUserRoleMaster/CompanyRoles/1").pipe(
+    return this.http.get<any>(environment.companyUserRoleMasterBaseUrl + "/CompanyRoles/"+this.userRoleSettings.companyId).pipe(
+   // return this.http.get<any>("https://localhost:7047/api/CompanyUserRoleMaster/CompanyRoles/1").pipe(
       catchError(error => {
         console.error('Get custom company role failed');
         return of(error);  // Return false on error
@@ -79,8 +79,8 @@ export class RolemasterService {
       CompanyId: customRoleDefinition.companyId,
       PermissionsAssigned:customRoleDefinition.permisionsAssigned
     };
-    //return this.http.post<any>(environment.companyUserRoleMasterBaseUrl + "/create", customRole).pipe(
-    return this.http.post<any>("https://localhost:7047/api/CompanyUserRoleMaster/create", customRole).pipe(
+    return this.http.post<any>(environment.companyUserRoleMasterBaseUrl + "/create", customRole).pipe(
+    //return this.http.post<any>("https://localhost:7047/api/CompanyUserRoleMaster/create", customRole).pipe(
       map((response: { id: number; }) => {
         console.log(response);
         return (response.id);
@@ -100,8 +100,8 @@ export class RolemasterService {
       CompanyId: customRoleDefinition.companyId,
       PermissionsUpdated : customRoleDefinition.permisionsAssigned
     };
-    //return this.http.post<any>(environment.companyUserRoleMasterBaseUrl + "/update", customRole).pipe(
-      return this.http.put<any>("https://localhost:7047/api/CompanyUserRoleMaster/update", customRole).pipe(
+    return this.http.post<any>(environment.companyUserRoleMasterBaseUrl + "/update", customRole).pipe(
+    //  return this.http.put<any>("https://localhost:7047/api/CompanyUserRoleMaster/update", customRole).pipe(
       map((response: { id: number; }) => {
         console.log(response);
         return (response.id);
