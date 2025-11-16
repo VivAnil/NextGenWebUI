4a525b97921fc1a95e0cfc30e1efc14f96c930c4
Vivek Anil <vivek.anil@defindia.org>
2025-11-16 20:58:56 +0530

    Fixed UI issue in  BusinessProduct

--- src/app/components/businessproduct/businessproduct.component.ts
+++ src/app/components/businessproduct/businessproduct.component.ts
@@ -14,7 +14,17 @@ export class BusinessproductComponent implements OnInit {
   roleId: any;
   companyRoleId: any;
   userName: any;
-
+  displayTab: string = 'block';
+  displayTab1: string = 'none';
+  displayFilter: string = 'none';
+  activeFilter: string = 'filter-link';
+  isFilterOpen: boolean = false;
+  displayColumn: string = 'none';
+  activeColumn: string = 'column-link';
+  activeDefault: string = 'default-link';
+  activeTab: string = 'ui-tab ui-tabs-active ui-state-active';
+  activeTab1: string = 'ui-tab ';
+  isColumnOpen: boolean = false;
   rweBusinessfilters: RWEBusinessFilters | undefined;
 
   clients = [
@@ -347,5 +357,62 @@ clients3 = [
   loadDashboard() {
     //this.companyId
     this.router.navigate(['dashboard/' + this.roleId]);
+  }
+  openFilter() {
+    this.isFilterOpen = !this.isFilterOpen;
+    if (this.displayFilter == 'none') {
+      this.displayFilter = 'block';
+      this.activeFilter = 'filter-link filter-tab-btn';
+
+    }
+    else {
+      this.displayFilter = 'none';
+      this.activeFilter = 'filter-link';
+    }
+  }
+  openColumn() {
+    this.isColumnOpen = !this.isColumnOpen;
+
+    if (this.displayColumn == 'none') {
+      this.displayColumn = 'block';
+      this.activeColumn = 'column-link filter-tab-btn';
+
+    }
+    else {
+      this.displayColumn = 'none';
+      this.activeColumn = 'column-link';
+    }
+  }
+
+  openDefault() {
+
+    if (this.activeDefault == 'default-link') {
+
+      this.activeDefault = 'default-link default-tab-btn';
+
+    }
+    else {
+
+      this.activeDefault = 'default-link';
+    }
+
+  }
+  toggleTab() {
+    if (this.displayTab == 'none') {
+      this.displayTab = 'block';
+      this.activeTab = 'ui-tab ui-tabs-active ui-state-active';
+    }
+    else {
+      this.displayTab = 'none';
+      this.activeTab = 'ui-tab';
+    }
+    if (this.displayTab1 == 'none') {
+      this.displayTab1 = 'block';
+      this.activeTab1 = 'ui-tab ui-tabs-active ui-state-active';
+    }
+    else {
+      this.displayTab1 = 'none';
+      this.activeTab1 = 'ui-tab';
+    }
   }
 }
